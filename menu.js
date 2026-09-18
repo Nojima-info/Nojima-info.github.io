49d3f11bf8e11480d86e8184780bdd65ece4535b
Nojima-info <noji.fami0316@gmail.com>
2026-09-18 13:01:23 +0900

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,1 @@
+const menuButton=document.querySelector(".menu-button");const menu=document.querySelector(".menu");if(menuButton&&menu){menuButton.addEventListener("click",()=>{const isOpen=menu.classList.toggle("open");menuButton.setAttribute("aria-expanded",String(isOpen));});}
